6adfb43e4d63c329c0732733bef541e1126c9e51
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-09-17 20:46:23 +0700

    Initialize Assignment 1

---
+++ assignment-1/app/src/main/java/id/ac/ui/cs/mobileprogramming/assignment1/MainActivity.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.mobileprogramming.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
