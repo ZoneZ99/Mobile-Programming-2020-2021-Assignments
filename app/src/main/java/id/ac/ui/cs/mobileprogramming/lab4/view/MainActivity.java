817231c6fc86c578d2947cf9f72177c7e5da6af6
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-10-07 23:22:34 +0700

    Change project packages structure and add data POJO

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/lab4/view/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/lab4/view/MainActivity.java
@@ -1,7 +1,8 @@
-package id.ac.ui.cs.mobileprogramming.lab4;
+package id.ac.ui.cs.mobileprogramming.lab4.view;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import id.ac.ui.cs.mobileprogramming.lab4.R;
 
 public class MainActivity extends AppCompatActivity {
 
