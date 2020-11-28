b2663b77c0d61a04afc5745a1cca2b20cace8dbb
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-11-28 09:27:49 +0700

    Create layout

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/lab5/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/lab5/MainActivity.java
@@ -1,13 +1,19 @@
 package id.ac.ui.cs.mobileprogramming.lab5;
 
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import id.ac.ui.cs.mobileprogramming.lab5.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+	private ActivityMainBinding mBinding;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+		View view = mBinding.getRoot();
+		setContentView(view);
 	}
 }
